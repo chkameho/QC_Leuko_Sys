e486f560f107f018f2191c90a0f8916a044abd35
chkameho <131600536+chkameho@users.noreply.github.com>
2023-04-24 22:43:06 +0200

    Update jsonbin.py

--- jsonbin.py
+++ jsonbin.py
@@ -2,13 +2,13 @@ import requests
 
 BIN_API_URL = r'https://api.jsonbin.io/v3/b'
 
-def load_data(api_key, bin_id):
+def load_data_jsonbin(api_key, bin_id):
     url = BIN_API_URL + '/' + bin_id + '/latest'
     headers = {'X-Master-Key': api_key}
     res = requests.get(url, headers=headers).json()
     return res['record']
 
-def save_data(api_key, bin_id, data):
+def save_data_jsonbin(api_key, bin_id, data):
     url = BIN_API_URL + '/' + bin_id
     headers = {'X-Master-Key': api_key, 'Content-Type': 'application/json'}
     res = requests.put(url, headers=headers, json=data).json()
